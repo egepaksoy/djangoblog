6b2f98ccc098119a0a3444cc7c3e7d5a62683b61
Ege Paksoy <egepaskoy@gmail.com>
2021-02-27 12:42:35 +0300

    secret key kaldırdım

--- blogpro/settings.py
+++ blogpro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '76)8$40*ox*f9+lef89ffza7l1g0mkv+fc(gl*elar4a6y=s=@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
